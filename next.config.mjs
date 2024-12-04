f69c5e317534afd1866807cd286c8376d97e1867
wagolemusa <homiemusa@gmail.com>
2024-12-04 07:36:05 +0300

    Add url

--- next.config.mjs
+++ next.config.mjs
@@ -16,8 +16,8 @@
 // "https://testgasapp-fe8069d27cfc.herokuapp.com",
 const nextConfig = {
     env: {
-        ENVIRONMENT_URL: "http://localhost:3000",
-        NEXTAUTH_URL: "http://localhost:3000",
+        ENVIRONMENT_URL: "https://gasmen-7852478929a9.herokuapp.com",
+        NEXTAUTH_URL: "https://gasmen-7852478929a9.herokuapp.com",
         DB_URL: "mongodb+srv://homiemusa:djrefuge@cluster0.fk517ja.mongodb.net/npcdatabase?retryWrites=true&w=majority", 
         CLOUND_NAME:"xycoders",
         CLOUDINARY_API_KEY: "837887428819121",
