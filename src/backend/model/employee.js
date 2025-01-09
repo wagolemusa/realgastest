cdacf8e4523441a4fea07ae4b03b24f92c731ef8
Your Name <you@example.com>
2025-01-09 22:18:21 +0300

    Create employee data

---
+++ src/backend/model/employee.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    sex: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+    idnumber: {
+        type: String,
+        required: true
+    },
+    nextOfKinName: {
+        type: String,
+        required: false 
+    },
+    nextOfKinPhone: {
+        type: Number,
+        required: false 
+    }
+
+}, {timestamps: true});
+
+export default mongoose.models.Employee ||
+ mongoose.model('Employee', employeeSchema)
