8f418bedab49d93111ea796a73e4eb4dfa72c214
Your Name <you@example.com>
2025-01-25 18:36:06 +0300

    Modificatioons

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -9,6 +9,18 @@ const Profile = () => {
 
 <h2 class="mb-2 text-lg font-semibold lg:text-2xl pb-3 pt-6  text-gray-700 ">All In One Gas Software System:</h2>
 <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-500 lg:text-2xl">
+<li class="flex items-center">
+        <svg class="w-3.5 h-3.5 me-2 text-green-600 dark:text-green-600 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+         Online Shoping 
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 me-2 text-green-600 dark:text-green-600 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+         Manage Orders
+    </li>
     <li class="flex items-center">
         <svg class="w-3.5 h-3.5 me-2 text-green-600 dark:text-green-600 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
